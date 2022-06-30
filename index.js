333c9b6cc97db59d70578e4dc91e4278c27bae52
Chloe Kurdziel <chloekurdziel@Chloes-MacBook-Pro.local>
2022-06-30 19:35:37 -0400

    Final Changes

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ header.style.textAlign = "left"
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
 	dogImages[i].style.borderRadius = '50px'
+    //dogImages[i].style.transform: rotate(180deg);
 }
 let dogNames = document.querySelectorAll('.dog-name')
 for(let i = 0; i < dogNames.length; i++){
     dogNames[i].style.textAlign = "left"
 }
+let footer = document.querySelector('.footer')
+footer.style.color = 'Blue'
 
 // Select some elements...
